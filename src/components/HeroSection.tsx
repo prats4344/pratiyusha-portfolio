58eb1592eb81a2e02c1244701604fe6f32c87267
Pratiyusha Kanungo <kanungopratiyusha@gmail.com>
2025-09-05 22:34:25 +0530

    HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -117,7 +117,7 @@ const HeroSection = () => {
                 rel="noopener noreferrer"
               >
                 <Download className="mr-2 h-5 w-5" />
-                Download Resume
+                View Resume
               </a>
             </Button>
           </motion.div>
