bf02837dc760c0434bc9be0bb04c7d8debc830c5
Pratiyusha Kanungo <kanungopratiyusha@gmail.com>
2025-09-06 10:14:25 +0530

    Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -57,7 +57,7 @@ const Navigation = () => {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              Pratiyusha
+              Portfolio
             </motion.button>
 
             {/* Desktop Navigation */}
